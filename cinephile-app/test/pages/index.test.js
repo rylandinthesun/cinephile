abc90460d0165c50159bd699343dc6b5a08f6d5e
Ryland Oehlers <ryland.oehlers@gmail.com>
2021-07-27 17:01:33 -0400

    homepage tests added

--- cinephile-app/test/pages/index.test.js
+++ cinephile-app/test/pages/index.test.js
@@ -3,8 +3,15 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Home from '../../pages/index';
 
-test('should render homepage and signin link', () => {
-	render(<Home />);
-	const signInLink = screen.getByText('Sign In');
-	expect(signInLink).toBeInTheDocument();
+describe('Homepage', () => {
+	test('should render homepage and signin link for non signed in user', () => {
+		render(<Home />);
+		const signInLink = screen.getByText('Sign In');
+		const header = screen.getByRole('heading', { name: 'Cine Phile' });
+
+		expect(signInLink).toBeInTheDocument();
+		expect(signInLink).toHaveClass('signIn');
+		expect(header).toBeInTheDocument();
+		expect(header).toHaveClass('title');
+	});
 });
