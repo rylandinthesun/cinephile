86ca91d9534c09dbae73e96c4cfa9d161f6ce6f5
Ryland Oehlers <ryland.oehlers@gmail.com>
2021-08-02 12:25:49 -0400

    service key change

--- cinephile-app/firebase/firebaseAdmin.js
+++ cinephile-app/firebase/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+const serviceAccount = require('../serviceKey.json');
 
 export const verifyIdToken = (token) => {
 	if (!admin.apps.length) {
