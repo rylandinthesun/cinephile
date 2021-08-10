dbce72ee4aeac1286ef3091ea4a662c60d3aec9b
Ryland Oehlers <ryland.oehlers@gmail.com>
2021-08-10 13:20:12 -0400

    sasdasdasd

--- cinephile-app/firebase/firebaseAdmin.js
+++ cinephile-app/firebase/firebaseAdmin.js
@@ -1,8 +1,12 @@
 import admin from 'firebase-admin';
 
-const google_credentials = JSON.parse(Buffer.from(process.env.GCLOUD_CREDENTIALS, 'base64'));
+// const google_credentials = JSON.parse(Buffer.from(process.env.GCLOUD_CREDENTIALS, 'base64'));
 
 export const verifyIdToken = (token) => {
+	const buff = Buffer(process.env.GCLOUD_CREDENTIALS, 'base64');
+	const gcp = buff.toString('ascii');
+	const google_credentials = JSON.parse(gcp);
+
 	if (!admin.apps.length) {
 		admin.initializeApp({
 			credential  : admin.credential.cert(google_credentials),
