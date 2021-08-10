e9991b1d293ed5ea7d204bbefbaa9dbb30b3d7a6
Ryland Oehlers <ryland.oehlers@gmail.com>
2021-08-10 13:30:20 -0400

    workkkkkk plz

--- cinephile-app/pages/signin.js
+++ cinephile-app/pages/signin.js
@@ -2,7 +2,6 @@ import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import { useAuth } from '../firebase/auth';
 import joinStyles from '../styles/Join.module.css';
 
 const uiConfig = {
@@ -14,15 +13,11 @@ const uiConfig = {
 };
 
 const SignIn = () => {
-	const { user } = useAuth();
-
 	return (
 		<div className={joinStyles.container}>
-			{!user ? (
-				<div className={joinStyles.firebaseBtns}>
-					<StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-				</div>
-			) : null}
+			<div className={joinStyles.firebaseBtns}>
+				<StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+			</div>
 		</div>
 	);
 };
